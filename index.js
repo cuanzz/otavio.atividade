79ea5f16ad37acc6cb7cd9476db9206e08d7d171
cauã <139181395+cuanzz@users.noreply.github.com>
2023-11-16 10:52:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+function calcularimc() {
+    
+const nome = prompt ('digite seu nome');
+const peso = parseFloat(prompt('digite o seu peso'))
+const altura = parseFloat(prompt('digite a altura'))
+
+
+
+
+if (imc < 16.0) 
+
+      m;
+     if (I < 16.0)
+     {
+         m = "Magreza grau III";
+     }
+     else if (I < 17.0)
+     {
+        m = "Magreza grau II";
+     }
+     else if (I < 18.5)
+     {
+        m = "Magreza grau I";
+     }
+    else if (I < 25.0)
+     {
+        m = "Peso adequado";
+     }
+    else if (I < 30.0)
+     {
+        m = "Pré obeso";
+     }
+    else if (I < 35.0)
+     {
+        m = "Obesidade grau I";
+     }
+    else if (I < 40.0)
+     {
+        m = "Obesidade grau II";
+     }
+   else if (I >= 40.0)
+     {
+        m = "Obesidade grau III";
+     }
+     else
+     {
+        m = "Inválido";
+     }
+     
+
+
+
+
+
+
+
+
+
+
+}
